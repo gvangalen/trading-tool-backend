3edeb5e333fd1dc53910174868c0219584a0a3ae
gvangalen <gvangalen@hotmail.com>
2025-06-08 09:28:52 +0200

    Update technical_data_api.py

--- backend/api/technical_data_api.py
+++ backend/api/technical_data_api.py
@@ -6,7 +6,7 @@ from celery import Celery
 from utils.db import get_db_connection
 from utils.technical_interpreter import process_technical_indicator
 
-router = APIRouter(prefix="/api")
+router = APIRouter()
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
