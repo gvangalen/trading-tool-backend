c1f1e79d05788deb86f33843b26a48b826328eaf
gvangalen <gvangalen@hotmail.com>
2025-07-07 21:15:55 +0200

    Update report_api.py

--- backend/api/report_api.py
+++ backend/api/report_api.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fastapi import APIRouter, HTTPException, Query
 from fastapi.responses import StreamingResponse
 
-from utils.db import get_db_connection
+from backend.utils.db import get_db_connection
 from utils.pdf_generator import generate_pdf_report
 from celery_task.daily_report_task import generate_daily_report
 
