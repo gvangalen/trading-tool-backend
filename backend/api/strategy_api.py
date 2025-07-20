d7c5c1b13a36d51858b9d2949c2b2c5f64d26610
gvangalen <gvangalen@hotmail.com>
2025-07-20 14:08:31 +0200

    Update strategy_api.py

--- backend/api/strategy_api.py
+++ backend/api/strategy_api.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import StreamingResponse
 from backend.utils.db import get_db_connection
 from celery_task.strategy_task import generate_strategy_task
-from backend.utils.ai_strategy_utils import generate_strategy, generate_strategy_from_setup
+from backend.utils.ai_strategy_utils import generate_strategy_from_setup
 from typing import Optional
 from datetime import datetime
 import json
