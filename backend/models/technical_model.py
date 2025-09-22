264b2171aea9358ee517c5851a3464cd98beff5a
gvangalen <gvangalen@hotmail.com>
2025-09-22 17:33:52 +0200

    Create technical_model.py

---
+++ backend/models/technical_model.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class TechnicalIndicator(BaseModel):
+    symbol: str
+    indicator: str
+    value: float
+    score: int
+    advies: str
+    uitleg: str
+    timestamp: datetime
