2d87ff666f7267afb4e1d0447cb888ad56051f37
gvangalen <gvangalen@hotmail.com>
2025-11-18 11:07:54 +0100

    Update technical_data_api.py

--- backend/api/technical_data_api.py
+++ backend/api/technical_data_api.py
@@ -1,6 +1,6 @@
 import logging
 from datetime import datetime
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 from backend.utils.db import get_db_connection
 
 router = APIRouter()
