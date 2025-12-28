3a39d15056d5d5158eb149a9c4850fcc81f6afc3
gvangalen <gvangalen@hotmail.com>
2025-12-28 19:22:10 +0100

    Create ai_style.py

---
+++ backend/ai_core/ai_style.py
@@ -0,0 +1,20 @@
+AI_STYLE = """
+Schrijf:
+- compact
+- zakelijk
+- zonder uitleg van basisbegrippen
+
+Gebruik vaste labels:
+- ACTIE
+- STATUS
+- RISICO
+- IMPACT
+- BESLISSING
+
+Vermijd:
+- hype
+- onzeker taalgebruik
+- open eindes
+
+Sluit elke output af met een duidelijke conclusie.
+"""
